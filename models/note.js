c574589130b4cef4ae679fbe33fd73584cb3f51e
hamini <ahmademin321@gmail.com>
2023-11-26 23:44:48 +0300

    Added content validator.

--- models/note.js
+++ models/note.js
@@ -12,7 +12,11 @@ mongoose.connect(url).then(res=>{
 })
 
 const noteSchema = mongoose.Schema({
-    content : String,
+    content : {
+        type:String,
+        minLength:5,
+        required:true
+    },
     important : Boolean
 })
 
