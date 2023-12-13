91a4672405228f81b21d9193c542aac39440a650
hamini <ahmademin321@gmail.com>
2023-12-13 22:23:54 +0300

    Updated note_api

--- tests/note_api.test.js
+++ tests/note_api.test.js
@@ -5,23 +5,13 @@ const supertest = require ('supertest')
 const app = require ('../app')
 const api = supertest (app)
 const Note = require('../models/note')
-
-const initialize =[
-	{
-	  content: 'HTML is easy',
-	  important: false,
-	},
-	{
-	  content: 'Browser can execute only JavaScript',
-	  important: true,
-	},
-]
+const testHelper = require('./test_helper')
 
 beforeEach(async () => {
 	await Note.deleteMany({})
-	let newNote = new Note(initialize[0])
+	let newNote = new Note(testHelper.initialObjects[0])
 	await newNote.save()
-	newNote = new Note(initialize[1])
+	newNote = new Note(testHelper.initialObjects[1])
 	await newNote.save()
 }, 100000)
 
@@ -30,8 +20,9 @@ test('testing the content validation', async() => {
 		important:true
 	}
 	await api.post('/api/notes').send(newObj).expect(400)
-	const notes = await api.get('/api/notes')
-	expect(notes.body).toHaveLength(initialize.length)
+	const notes = await testHelper.notesInDatabase()
+	console.log(notes)
+	expect(notes).toHaveLength(testHelper.initialObjects.length)
 }, 100000)
 
 test('Testing the get with async function', async() => {
@@ -42,27 +33,27 @@ test('Testing the get with async function', async() => {
 	await api.post('/api/notes')
 		.send(newObj).expect(201).expect('Content-Type', /application\/json/)
 
-	const notes = await api.get('/api/notes')
-	const contents = notes.body.map(item => item.content)
-	expect(notes.body).toHaveLength(initialize.length+1)
+	const notes = await testHelper.notesInDatabase()
+	const contents = notes.map(item => item.content)
+	expect(notes).toHaveLength(testHelper.initialObjects.length+1)
 	expect(contents).toContain('newcontent')
 }, 100000)
 
 test('there are two notes in test db', async () => {
-	const res = await api.get('/api/notes')
-	expect(res.body).toHaveLength(2)
-}, 100000)
+	const res = await testHelper.notesInDatabase()
+	expect(res).toHaveLength(testHelper.initialObjects.length)
+}, 10000)
 
 test('first item content is html is easy as fuck...', async () => {
-	const res = await api.get('/api/notes')
-	expect(res.body[0].content).toBe('HTML is easy')
-}, 100000)
+	const res = await testHelper.notesInDatabase()
+	expect(res[0].content).toBe('HTML is easy')
+}, 10000)
 
 test('notes returned as json',async () => {
 	await api.get('/api/notes')
 		.expect(200)
 		.expect('Content-Type', /application\/json/)
-}, 100000)
+}, 10000)
 
 afterAll(async () => {
 	await mongoose.connection.close()
