a6037e7d5258062b331b3b76903d1e75feb767db
hamini <ahmademin321@gmail.com>
2023-12-21 01:46:34 +0300

    added the mongoose populate to user and notes

--- controllers/notes.js
+++ controllers/notes.js
@@ -1,13 +1,19 @@
 const notesRouter = require ('express').Router()
 const Note = require ('../models/note')
+const User = require('../models/user')
+
 
 notesRouter.post ('/',async(req,res) => {
 	const data = req.body
+	const user = await User.findById(data.userId)
 	const note = new Note({
 		content: data.content,
-		important: data.important||false
+		important: data.important||false,
+		user:user.id
 	})
 	const savedNote = await note.save()
+	user.notes = user.notes.concat(savedNote._id)
+	await user.save()
 	res.status(201).json(savedNote)
 })
 
@@ -36,7 +42,7 @@ notesRouter.delete ('/:id',async(req,res) => {
 })
 
 notesRouter.get ('/',async(req,res) => {
-	const notes=await Note.find({})
+	const notes=await Note.find({}).populate('user',{ username:1,name:1 })
 	res.json(notes)
 })
 
